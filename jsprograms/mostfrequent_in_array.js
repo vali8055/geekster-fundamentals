a49c2612e9211db319812e9638aa6ae54f640cd6
vali8055 <91729905+vali8055@users.noreply.github.com>
2021-12-11 13:22:04 +0530

    Add files via upload

---
+++ jsprograms/mostfrequent_in_array.js
@@ -0,0 +1,19 @@
+var arr1=[1,2,3,3,3,3,5];
+var mf = 1;
+var m = 0;
+var item;
+for (var i=0; i<arr1.length; i++)
+{
+        for (var j=i; j<arr1.length; j++)
+        {
+                if (arr1[i] == arr1[j])
+                 m++;
+                if (mf<m)
+                {
+                  mf=m; 
+                  item = arr1[i];
+                }
+        }
+        m=0;
+}
+console.log(item+" ( " +mf +" times ) ") ;
